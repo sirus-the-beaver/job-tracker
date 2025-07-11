db7f524952f318837024c59f0e83ce6d397b6c0d
tmurr <34355872+tmurr@users.noreply.github.com>
2025-07-11 15:39:08 +0000

    react router

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -43,7 +43,7 @@ const Login = () => {
                     <h1>You are logged in!</h1>
                     <br />
                     <p>
-                        <a href="#">Go to Home</a>
+                        <a href="/">Go to Home</a>
                     </p>
                 </section>
             ) : (
@@ -76,7 +76,7 @@ const Login = () => {
                 Need an Account?<br />
                 <span className="line">
                     {/*put router link here*/}
-                    <a href="#">Sign Up</a>
+                    <a href="/">Sign Up</a>
                 </span>
             </p>
 
