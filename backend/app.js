8fb1306df4d0f06b2feb4387b5ab964c219486ae
Sirus Salari <salaris@oregonstate.edu>
2025-07-11 19:46:17 -0700

    Remove handlebars since we are using React for rendering the UI and HTML

--- backend/app.js
+++ backend/app.js
@@ -12,25 +12,6 @@ const PORT = 30239;
 // Database
 const db = require('./database/db-connector');
 
-// Handlebars
-const { engine } = require('express-handlebars'); // Import express-handlebars engine
-app.engine('.hbs', engine({ extname: '.hbs' })); // Create instance of handlebars
-app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
-
-// ########################################
-// ########## ROUTE HANDLERS
-
-// READ ROUTES
-app.get('/', async function (req, res) {
-    try {
-        res.render('home'); // Render the home.hbs file
-    } catch (error) {
-        console.error('Error rendering page:', error);
-        // Send a generic error message to the browser
-        res.status(500).send('An error occurred while rendering the page.');
-    }
-});
-
 
 // ########################################
 // ########## LISTENER
