8947fd53d696c82ec04f70cdff3e6d67552ae0a2
Sirus Salari <salaris@oregonstate.edu>
2025-07-11 20:22:57 -0700

    Remove username row from users table

--- backend/database/DDL.sql
+++ backend/database/DDL.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
     `user_id` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(250) NOT NULL,
     `email` VARCHAR(250) UNIQUE NOT NULL,
     `password` CHAR(60) NOT NULL,
     PRIMARY KEY (`user_id`)
