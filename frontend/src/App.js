fa2a7caac94b15aac675ce30950c3c80179fae07
Sirus Salari <salaris@oregonstate.edu>
2025-07-16 21:09:30 -0700

    Set up private routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,14 @@ Section: Steps to Create Routes using React Router
 Subsection: Example: This example demonstrates implemeting basic routes in a React App.
 URL: https://www.geeksforgeeks.org/reactjs/reactjs-router/ */
 
-/* Citation for lines 19 and 24:
+/* Citation for lines 19 and 24, 26-29:
 Date: 07/16/2025
 Source: https://dev.to/miracool/how-to-manage-user-authentication-with-react-js-3ic5
 Author(s): Makanju Oluwafemi */
 
 import { Routes, Route } from "react-router-dom";
-import AuthProvider from "./contexts/AuthContext";
+import AuthProvider from './contexts/AuthContext';
+import PrivateRoute from './contexts/PrivateRoute';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
@@ -20,8 +21,12 @@ function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          {/* Add private routes here */}
+          <Route path="/" element={<h1>Welcome to the Home Page</h1>} />
+        </Route>
       </Routes>
     </AuthProvider>
   );
