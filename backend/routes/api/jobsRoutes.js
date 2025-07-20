4c31ebe18dceba31462d3d9c04ff2c9d9fe181df
tmurr <34355872+tmurr@users.noreply.github.com>
2025-07-20 20:00:43 +0000

    add POST, PUT, DELETE routes

--- backend/routes/api/jobsRoutes.js
+++ backend/routes/api/jobsRoutes.js
@@ -8,16 +8,32 @@
 const express = require('express');
 const router = express.Router();
 
+// a language of verbs (such as GET, POST, PUT, & DELETE) describe what should happen
+
+
+// GET a specific resource (e.g., get all of the jobs a user has applied to)
 // GET route should basically return all of the jobs that a user has applied to
 router.get('/', (req, res) => {
     res.send('this is jobs route');
 });
 
+// POST: create a new resource (e.g., add a new job to the database)
 // POST route should receive data payload from frontend and store in DB
-
+router.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+// PUT: update an existing resource (or create a new one if it doesn't exist)
 // PUT route should receive data payload and update DB for that job ID
 
+router.put('/', (req, res) => {
+    res.send('Got a PUT request at /')
+})
+
+// DELETE the specificed resource
 // DELETE route should delete row from DB for that job ID
+router.delete('/', (req, res) => {
+    res.send('Got a DELETE request at /')
+})
 
 // export the router module so that other files can use it
 module.exports = router;
