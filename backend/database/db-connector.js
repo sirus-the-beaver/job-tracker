45a41a5043bddcba8b2c841174805556fabc74b3
Sirus Salari <salaris@oregonstate.edu>
2025-07-24 01:55:03 -0700

    Add ssl cert to db connector

--- backend/database/db-connector.js
+++ backend/database/db-connector.js
@@ -1,5 +1,7 @@
 // Get an instance of mysql we can use in the app
 let mysql = require('mysql2')
+require('dotenv').config();
+const fs = require('fs');
 
 // Create a 'connection pool' using the provided credentials
 const pool = mysql.createPool({
@@ -9,6 +11,11 @@ const pool = mysql.createPool({
     user              : `${process.env.DB_USER}`,
     password          : `${process.env.DB_PASSWORD}`,
     database          : `${process.env.DB_NAME}`,
+    port              : `${process.env.DB_PORT}`,
+    ssl               : {
+        ca: fs.readFileSync('ca.pem'),
+        rejectUnauthorized: true
+    }
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
 // Export it for use in our application
